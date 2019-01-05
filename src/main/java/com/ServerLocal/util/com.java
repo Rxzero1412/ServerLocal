2ac89bed705817a2757b62ad005e781eb5b6a294
83541 <835413189@qq.com>
2019-01-05 17:32:42 +0800

    20190105

--- src/main/java/com/ServerLocal/util/com.java
+++ src/main/java/com/ServerLocal/util/com.java
@@ -1,6 +1,8 @@
 package com.ServerLocal.util;
 
 import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -9,7 +11,7 @@ import java.util.List;
 /**
  * Created by 83541 on 2018/12/8.
  */
-public class com {
+public class com extends Thread implements SerialPortEventListener {
     public List<String> listPorts(){
         List<String> relist=new ArrayList<>();
         Enumeration enumeration= CommPortIdentifier.getPortIdentifiers();
@@ -18,10 +20,14 @@ public class com {
             portId=(CommPortIdentifier)enumeration.nextElement();
             if(portId.getPortType()==CommPortIdentifier.PORT_SERIAL){
                 System.out.println("port name :"+portId.getName());
-
                 relist.add(portId.getName());
             }
         }
         return relist;
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent serialPortEvent) {
+
     }
 }
